22aded46569cf9138ce39e08a666d0080b05dcab
Thorsten Otto <admin@tho-otto.de>
2017-12-13 16:56:48 +0100

    Remove dead code

--- engine/text_sq.s
+++ engine/text_sq.s
@@ -250,50 +250,6 @@ vst_effects:
 	used_d1
 	done_return
 
-  ifne 0
-* lib_vst_effects - Standard Library function
-* Todo: -
-* In:	a1	Parameters   effects_set = lib_vst_effects(effects)
-*	a0	VDI struct
-lib_vst_effects:
-	move.w	(a1),d0
-
-* Some other method should be used for this!
-	move.l	vwk_text_current_font(a0),a2
-	tst.w	font_flags(a2)
-	bpl	.no_external_vst_effects
-
-	movem.l	d2/a3,-(a7)
-
-	move.l	_external_vst_effects,d2	; (Handle differently?)
-	beq	.no_external_vst_effects	; Not really allowed!
-	move.l	d2,a3
-
-	move.l	a7,d2				; Give external renderer
-	move.l	_vdi_stack_top,a7		;  extra stack space!
-	move.l	d2,-(a7)			; (Should be improved)
-
-	movem.l	d0-d1/a0-a2,-(a7)
-	move.l	_vdi_stack_size,-(a7)
-	move.l	d0,-(a7)			; Effects to set
-	move.l	a2,-(a7) 			; Fontheader
-	move.l	a0,-(a7)			; Virtual
-	jsr	(a3)
-	add.w	#4*4,a7
-	movem.l	(a7)+,d0-d1/a0-a2
-
-	move.l	(a7),a7				; Return to original stack
-	movem.l	(a7)+,d2/a3
-	rts
-
-.no_external_vst_effects:
-	move.l	vwk_real_address(a0),a2
-	and.w	wk_writing_effects(a2),d0
-	move.w	d0,vwk_text_effects(a0)
-	rts
-  endc
-
-
 * vst_alignment - Standard Trap function
 * Todo: ?
 * In:   a1      Parameter block
@@ -424,18 +380,6 @@ vqt_name:
 	movem.l	(a7)+,d2/a1
 	move.l	intout(a1),a2
 	move.w	d0,(a2)
-  ifne 0
-	move.l	intin(a1),a2
-	move.w	(a2),d0
-	move.l	intout(a1),a1
-	pea	2(a1)
-	move.w	d0,-(a7)
-	move.l	a7,a1
-	bsr	lib_vqt_name
-	addq.l	#2,a7
-	move.l	(a7)+,a1
-	move.w	d0,-2(a1)
-  endc
 	used_d1
 	done_return
 
@@ -455,51 +399,6 @@ vqt_ext_name:
 	move.w	d0,-2(a1)
 	used_d1
 	done_return
-
-  ifne 0
-* lib_vqt_name - Standard Library function
-* Todo: ?
-* In:	a1	Parameters   id = lib_vqt_name(number, name)
-*	a0	VDI struct
-lib_vqt_name:
-	move.w	(a1),d0
-	move.l	vwk_real_address(a0),a2
-	tst.w	d0
-	lbeq	.not_ok,1
-	cmp.w	wk_writing_fonts(a2),d0
-	lbls	.ok,2
- label .not_ok,1
-	moveq	#1,d0
- label .ok,2
-	subq.w	#1,d0
-	move.l	wk_writing_first_font(a2),a2
-	lbra	.loopend,4
- label .loop,3
-	move.l	font_next(a2),a2
- label .loopend,4
-	ldbra	d0,.loop,3
-
-	move.l	2(a1),a1
-	move.w	font_id(a2),a0
-
-	move.w	#0,2*32(a1)	; Assume bitmap font
-	tst.w	font_flags(a2)
-	lbpl	.bitmap,5
-	move.w	#1,2*32(a1)	; Vector font!
- label .bitmap,5
-
-	lea	font_name(a2),a2
-	moveq	#31,d1
-	moveq	#0,d0
- label .name,6
-	move.b	(a2)+,d0
-	move.w	d0,(a1)+
-	ldbra	d1,.name,6
-
-	move.l	a0,d0
-	rts
-  endc
-
 
 * lib_vqt_ext_name - Standard Library function
 * Todo: Does not yet know about different kinds of vector fonts etc.
@@ -570,33 +469,6 @@ vqt_font_info:
 	move.w	font_distance_top(a0),(a2)+
 	done_return
 
-  ifne 0
-* lib_vqt_font_info - Standard Library function
-* Todo: ?
-* In:	a1	Parameters   lib_vqt_font_info(&minchar, &maxchar, distance, &maxwidth, effects)
-*	a0	VDI struct
-lib_vqt_font_info:
-	move.l	vwk_text_current_font(a0),a0	; a0 no longer -> VDI struct!
-	move.l	(a1)+,a2
-	move.w	font_code_low(a0),(a2)
-	move.l	(a1)+,a2
-	move.w	font_code_high(a0),(a2)
-	move.l	(a1)+,a2
-	move.w	font_distance_bottom(a0),(a2)+
-	move.w	font_distance_descent(a0),(a2)+
-	move.w	font_distance_half(a0),(a2)+
-	move.w	font_distance_ascent(a0),(a2)+
-	move.w	font_distance_top(a0),(a2)
-	move.l	(a1)+,a2
-	move.w	font_widest_cell(a0),(a2)
-	move.l	(a1)+,a2
-	move.w	#0,(a2)+		; Temporary current spec. eff. change of width!
-	move.w	#0,(a2)+		; Temporary current spec. eff. change to left!
-	move.w	#0,(a2)			; Temporary current spec. eff. change to right!
-	rts
-  endc
-
-
 * vqt_fontheader - Standard Trap function
 * Todo:	?
 * In:   a1      Parameter block
@@ -646,140 +518,6 @@ vqt_xfntinfo:
 	move.w	(a2)+,(a1)
 	used_d1
 	done_return
-  ifne 0
-	uses_d1
-	move.l	a1,-(a7)
-	move.l	intin(a1),a1
-	bsr	lib_vqt_xfntinfo
-	move.l	(a7)+,a1
-	move.l	intin(a1),a2
-	move.l	6(a2),a2
-	addq.l	#4,a2
-	move.l	intout(a1),a1
-	move.l	(a2)+,(a1)+
-	move.w	(a2)+,(a1)
-	used_d1
-	done_return
- endc
-
- ifne 0
-* lib_vqt_xfntinfo - Standard Library function
-* Todo: More bits to test! Check size!
-* In:	a1	Parameters   lib_vqt_xfntinfo(flag, id, index, info)
-*	a0	VDI struct
-lib_vqt_xfntinfo:
-	move.l	d2,-(a7)
-	move.l	vwk_real_address(a0),a2
-	move.l	wk_writing_first_font(a2),a2
-
-	move.w	2(a1),d0
-	lbne	.id_ok,1
-	move.w	4(a1),d1
-	lbne	.index_ok,6
-
-	moveq	#1,d0
-	moveq	#1,d1
-	move.l	vwk_text_current_font(a0),d2
-	lbeq	.found,5
-	move.w	font_id(a2),d0
-
- label .id_ok,1
-	moveq	#0,d1
- label .search,2
-	addq.w	#1,d1
-	cmp.w	font_id(a2),d0
-	lbls	.maybe,3
-	move.l	font_next(a2),a2
-	move.l	a2,d2
-	lbne	.search,2
-	lbra	.not_found,4
- label .maybe,3
-	lbne	.not_found,4
-	moveq	#1,d2
-	lbra	.found,5
-
- label .index_ok,6
-	move.w	d1,d0
- label .search2,7
-	subq.w	#1,d1
-	lbeq	.counted,8
-	move.l	font_next(a2),a2
-	move.l	a2,d2
-	lbne	.search2,7
-	lbra	.not_found,4
- label .counted,8
-	move.w	d0,d1
-	move.w	font_id(a2),d0
-	moveq	#1,d2
-	lbra	.found,5
-
- label .not_found,4
-	moveq	#0,d0
-	moveq	#0,d1
-	moveq	#0,d2
- label .found,5
-	swap	d2
-	move.w	d0,d2
-	move.w	(a1),d0
-	move.l	6(a1),a1
-	addq.l	#4,a1
-	move.l	d2,(a1)+
-	move.w	d1,(a1)+
-	lbeq	.end,9
-
-	move.l	a2,d2
-	btst	#0,d0
-	lbeq	.l10,10
-	move.l	d2,a2
-	lea	font_name(a2),a2
-	swap	d0
-	moveq	#16-1,d0
- label .loop1,11
-	move.w	(a2)+,d1
-	move.b	d1,(a1)+
-	ldbra	d0,.loop1,11
-	swap	d0
-	move.b	#0,(a1)+
-	add.w	#50-17,a1
-
- label .l10,10
-	btst	#1,d0
-	lbeq	.l12,12
-	move.l	d2,a2
-	lea	font_name(a2),a2
-	swap	d0
-	moveq	#8-1,d0
- label .loop2,13
-	move.w	(a2)+,d1
-	move.b	d1,(a1)+
-	ldbra	d0,.loop2,13
-	swap	d0
-	move.b	#0,(a1)+
-	add.w	#50-9,a1
-
- label .l12,12
-	btst	#2,d0
-	lbeq	.l14,14
-	move.l	d2,a2
-	lea	font_name(a2),a2
-	add.w	#16,a2
-	swap	d0
-	moveq	#8-1,d0
- label .loop3,15
-	move.w	(a2)+,d1
-	move.b	d1,(a1)+
-	ldbra	d0,.loop3,15
-	swap	d0
-	move.b	#0,(a1)+
-	add.w	#50-9,a1
-
- label .l14,14
-
- label .end,9
-	move.l	(a7)+,d2
-	rts
- endc
-
 
 * vqt_extent - Standard Trap function
 * Todo:	The rest of the text modes
@@ -899,111 +637,6 @@ vqt_extent:
 	movem.l	(a7)+,d2-d4/a3-a4
 	used_d1
 	done_return
-
-
-  ifne 0
-* lib_vqt_extent - Standard Library function
-* Todo: ?
-* In:	a1	Parameters   lib_vqt_extent(length, &string, points)
-*	a0	VDI struct
-_get_extent:
-	move.l	4(a7),a0		; vwk as parameter
-	lea	8(a7),a1
-lib_vqt_extent:
-	movem.l	d2-d4/a3-a4,-(a7)
-	move.w	(a1),d0			; Number of characters
-	move.l	6(a1),a2
-	move.l	2(a1),a1
-	move.l	vwk_text_current_font(a0),a4
-
-* Some other method should be used for this!
-	tst.w	font_flags(a4)
-	lbmi	.external_vqt_extent,8
-
-	move.l	font_table_character(a4),a3
-	move.w	font_code_low(a4),d3
-	move.w	font_code_high(a4),d4
-
-	moveq	#0,d2			; Width total
-;	subq.w	#1,d0
-	lbra	.no_char,2
- label .loop,1
-	move.w	(a1)+,d1
-	sub.w	d3,d1			; Negative numbers are higher
-	cmp.w	d4,d1			;  than code_high
-	lbhi	.no_char,2
-	add.w	d1,d1
-	add.w	2(a3,d1.w),d2
-	sub.w	0(a3,d1.w),d2
- label .no_char,2
-	ldbra	d0,.loop,1
-
-	move.w	vwk_text_effects(a0),d0
-	btst	#0,d0
-	lbeq	.no_bold,5
-	add.w	font_thickening(a4),d2
- label .no_bold,5
-	btst	#4,d0
-	lbeq	.no_outline,6
-	addq.w	#2,d2
- label .no_outline,6
-	btst	#2,d0
-	lbeq	.no_italic,7
-	move.w	font_skewing(a4),d1
-	move.w	font_height(a4),d0
-	subq.w	#1,d0
- label .loop2,3
-	rol.w	#1,d1
-	lbcc	.skip,4
-	addq.w	#1,d2
- label .skip,4
-	ldbra	d0,.loop2,3
- label .no_italic,7
-
-	move.l	#0,(a2)+
-  ifne SUB1
-	subq.w	#1,d2
-  endc
-	swap	d2
-	move.l	d2,(a2)+
-	move.w	font_height(a4),d2
-  ifne SUB1
-	subq.w	#1,d2
-  endc
-	move.l	d2,(a2)+
-	ext.l	d2
-	move.l	d2,(a2)+
-
-	movem.l	(a7)+,d2-d4/a3-a4
-	rts
-
- label .external_vqt_extent,8
-	move.l	_external_vqt_extent,d2		; (Handle differently?)
-	lbeq	.no_external_vqt_extent,8	; Not really allowed!
-	move.l	d2,a3
-	
-	move.l	a7,d2				; Give external renderer
-	move.l	_vdi_stack_top,a7		;  extra stack space!
-	move.l	d2,-(a7)			; (Should be improved)
-
-	movem.l	d0-d1/a0-a2,-(a7)
-	move.l	_vdi_stack_size,-(a7)	
-	ext.l	d0
-	move.l	d0,-(a7)
-	move.l	a1,-(a7)
-	move.l	a4,-(a7)			; Fontheader
-	jsr	(a3)
-	add.w	#4*4,a7
-	move.l	d0,d2
-	movem.l	(a7)+,d0-d1/a0-a2
-
-	move.l	(a7),a7				; Return to original stack
-	lbra	.no_italic,7
-
- label .no_external_vqt_extent,9
-	movem.l	(a7)+,d2-d4/a3-a4
-	rts
-  endc
 
 
 * vqt_width - Standard Trap function
@@ -1482,26 +1115,6 @@ vqt_attributes:
 	move.l	(a0)+,(a2)+		; Character height and width
 	move.l	(a0)+,(a2)+		; Cell height and width
 	done_return
-
-  ifne 0
-* lib_vqt_attributes - Standard Library function
-* Todo: ?
-* In:	a1	Parameters   lib_vqt_attributes(settings)
-*	a0	VDI struct
-lib_vqt_attributes:
-	move.l	(a1),a1
-	move.w	vwk_mode(a0),d0
-	lea	vwk_text(a0),a0		; a0 no longer -> VDI struct!
-	move.w	(a0),(a1)+		; Font
-	addq.l	#4,a0
-	move.l	(a0)+,(a1)+		; Foreground, rotation
-	move.l	(a0)+,(a1)+		; Horizontal and vertical alignment
-	move.w	d0,(a1)+		; Mode
-	move.l	(a0)+,(a1)+		; Character height and width
-	move.l	(a0)+,(a1)+		; Cell height and width
-	rts
-  endc
-
 
 * vst_load_fonts - Standard Trap function
 * Todo: ?
